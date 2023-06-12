79d99abeaf9d21172bf17f4d2ba4eb6334f7d99f
jarikomppa <jari.komppa@gmail.com>
2023-06-12 19:44:31 +0300

    more doc toolchain tweaks

--- docsrc/build.bat
+++ docsrc/build.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Building html..
-call asciidoctor --destination-dir ../doc/ dialogtree.adoc
+call asciidoctor -r asciidoctor-diagram --destination-dir ../doc/ dialogtree.adoc
 echo Building pdf..
-call asciidoctor-pdf --destination-dir ../doc/ dialogtree.adoc
+call asciidoctor-pdf -r asciidoctor-diagram --destination-dir ../doc/ dialogtree.adoc
 echo Building epub3..
-call asciidoctor-epub3 --destination-dir ../doc/ dialogtree.adoc
+call asciidoctor-epub3 -r asciidoctor-diagram --destination-dir ../doc/ dialogtree.adoc
 echo Building mobi..
-call asciidoctor-epub3 --destination-dir ../doc/ -a ebook-format=kf8 dialogtree.adoc
+call asciidoctor-epub3 -r asciidoctor-diagram --destination-dir ../doc/ -a ebook-format=kf8 dialogtree.adoc
 echo Done.
